5557da2ad19daa26ddcbcb7c89172e7973acc63d
Gage Eide <gageeide@gmail.com>
2021-08-08 21:16:38 -0500

    Added boilerplate server file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const config = {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false
+}
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mmo_finder", config);
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
