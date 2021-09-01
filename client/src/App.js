09c00a358f6f6cd0e15f687b1f602569aaf7a710
Gage Eide <gageeide@gmail.com>
2021-08-31 20:33:58 -0500

    Added call to server on client homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { List, ListItem } from '@material-ui/core';
+import API from './utils/api';
 
 function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    API.get('/api')
+      .then(res => {
+        setGames(res);
+      });
+  }, []);
+
+  console.log(games);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List>
+        
+      </List>
     </div>
   );
 }
