7d0a4c8f680f41ca6253dc4ab5d8baceec5fc5e0
Gage Eide <gageeide@gmail.com>
2021-08-31 21:00:34 -0500

    Added mock data to API call until I get CORS figured out

--- client/src/App.js
+++ client/src/App.js
@@ -11,11 +11,45 @@ function App() {
     //   console.log(res)
     //   setGames(res);
     // });
-    fetch('http://localhost:3001/api/mmo')
-      .then(res => {
-        console.log(res)
-        setGames(res);
-      });
+    setGames([
+      {
+        name: 'Guild Wars 2',
+        description: 'A pretty rad MMO',
+        genre: 'MMORPG',
+        release_date: new Date('August 28, 2012'),
+        pay_model: 'Buy To Play',
+        platforms: ['PC'],
+        pve: true,
+        pvp: true,
+        developers: ['ArenaNet'],
+        publishers: ['ArenaNet', 'NCSoft'],
+        tags: ['Story-Rich', 'Genre-bending', 'World Events', 'Level Scaling', 'Expansive PvP']
+      },{
+        name: 'World of Warcraft',
+        description: 'The Big Cheese',
+        genre: 'MMORPG',
+        release_date: new Date('November 23, 2004'),
+        pay_model: 'Subscription',
+        platforms: ['PC'],
+        pve: true,
+        pvp: true,
+        developers: ['Blizzard Entertainment'],
+        publishers: ['Blizzard Entertainment'],
+        tags: ['Grind-heavy', 'Rich End Game', 'Expansive PvP']
+      },{
+        name: 'The Elder Scrolls Online',
+        description: 'A fancy story-based RPG',
+        genre: 'MMORPG',
+        release_date: new Date('June 1, 2021'),
+        pay_model: 'Buy to Play',
+        platforms: ['PC'],
+        pve: true,
+        pvp: true,
+        developers: ['ZeniMax Online Studios'],
+        publishers: ['Bethesda Softworks'],
+        tags: ['Story-Rich', 'World Events', 'Level-scaling']
+      },
+    ])
   }, []);
 
   console.log(games);
@@ -23,7 +57,9 @@ function App() {
   return (
     <div className="App">
       <List>
-        
+        {games && games.map(game => (
+          <ListItem>{game.name}</ListItem>
+        ))}
       </List>
     </div>
   );
