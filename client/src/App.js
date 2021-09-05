ebf353f4dde9cd5dd8e01acbccd3f959235c17d9
Gage Eide <gageeide@gmail.com>
2021-09-04 23:08:23 -0500

    imported and used GameGrid in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,10 @@
-import { useState, useEffect } from 'react';
-import { List, ListItem } from '@material-ui/core';
+import GameGrid from './components/GameGrid';
 import API from './utils/api';
 
 function App() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    // API.get('/mmo')
-    // .then(res => {
-    //   console.log(res)
-    //   setGames(res);
-    // });
-    setGames([
-      {
-        name: 'Guild Wars 2',
-        description: 'A pretty rad MMO',
-        genre: 'MMORPG',
-        release_date: new Date('August 28, 2012'),
-        pay_model: 'Buy To Play',
-        platforms: ['PC'],
-        pve: true,
-        pvp: true,
-        developers: ['ArenaNet'],
-        publishers: ['ArenaNet', 'NCSoft'],
-        tags: ['Story-Rich', 'Genre-bending', 'World Events', 'Level Scaling', 'Expansive PvP']
-      },{
-        name: 'World of Warcraft',
-        description: 'The Big Cheese',
-        genre: 'MMORPG',
-        release_date: new Date('November 23, 2004'),
-        pay_model: 'Subscription',
-        platforms: ['PC'],
-        pve: true,
-        pvp: true,
-        developers: ['Blizzard Entertainment'],
-        publishers: ['Blizzard Entertainment'],
-        tags: ['Grind-heavy', 'Rich End Game', 'Expansive PvP']
-      },{
-        name: 'The Elder Scrolls Online',
-        description: 'A fancy story-based RPG',
-        genre: 'MMORPG',
-        release_date: new Date('June 1, 2021'),
-        pay_model: 'Buy to Play',
-        platforms: ['PC'],
-        pve: true,
-        pvp: true,
-        developers: ['ZeniMax Online Studios'],
-        publishers: ['Bethesda Softworks'],
-        tags: ['Story-Rich', 'World Events', 'Level-scaling']
-      },
-    ])
-  }, []);
-
-  console.log(games);
-
   return (
     <div className="App">
-      <List>
-        {games && games.map(game => (
-          <ListItem>{game.name}</ListItem>
-        ))}
-      </List>
+      <GameGrid />
     </div>
   );
 }
