6875c45b25c031a28873d6627133c0234abef4a6
Gage Eide <gageeide@gmail.com>
2021-09-05 22:09:58 -0500

    Got game properties rendering

--- client/src/components/GameCard/index.js
+++ client/src/components/GameCard/index.js
@@ -7,10 +7,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -18,10 +16,11 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 345,
+    width: 345,
   },
   media: {
     height: 0,
+    backgroundSize: '100%',
     paddingTop: '56.25%', // 16:9
   },
   expand: {
@@ -34,12 +33,52 @@ const useStyles = makeStyles((theme) => ({
   expandOpen: {
     transform: 'rotate(180deg)',
   },
-  avatar: {
-    backgroundColor: red[500],
-  },
 }));
 
-export default function GameCard() {
+const generatePropertyName = (key) => {
+  if (key === "pvp") return "PvP"
+  if (key === "pve") return "PvE"
+  const capitalize = s => (s && s[0].toUpperCase() + s.slice(1)) || "";
+
+  let propertyName = "";
+  const splitKeyArr = key.split("_");
+  splitKeyArr.length > 1 ?
+    splitKeyArr.forEach(word => propertyName += `${capitalize(word)} `) :
+    propertyName = capitalize(splitKeyArr[0]);
+
+  return propertyName.trim();
+}
+
+const generatePropertyValue = (value) => {
+  let propertyValue = "";
+
+  if (value instanceof Date) {
+    propertyValue = value.toISOString();
+  } else if (Array.isArray(value)) {
+    propertyValue = value.join(", ");
+  } else if (typeof value === 'boolean') {
+    value === true ? propertyValue = 'True' : propertyValue = 'False'
+  } else {
+    propertyValue = value;
+  }
+
+  return propertyValue;
+}
+
+const renderGameProperty = (property, value) => {
+  if ( property === 'name' || property === 'description' || property === 'logo' || property === 'genre' ) {
+    return;
+  }
+
+  const propertyName = generatePropertyName(property);
+  const propertyValue = generatePropertyValue(value);
+  return ( <>
+    <Typography paragraph>{propertyName}:</Typography>
+    <Typography paragraph>{propertyValue}</Typography>
+  </>)
+}
+
+export default function GameCard({ game }) {
   const classes = useStyles();
   const [expanded, setExpanded] = useState(false);
 
@@ -50,28 +89,22 @@ export default function GameCard() {
   return (
     <Card className={classes.root}>
       <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            R
-          </Avatar>
-        }
         action={
           <IconButton aria-label="settings">
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={game.name}
+        subheader={game.genre}
       />
       <CardMedia
         className={classes.media}
-        image="/static/images/cards/paella.jpg"
-        title="Paella dish"
+        image={game.logo}
+        title={`${game.name} logo`}
       />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+          {game.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -94,29 +127,9 @@ export default function GameCard() {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+          {Object.entries(game).map(([key, value]) => {
+            return renderGameProperty(key, value);
+          })}
         </CardContent>
       </Collapse>
     </Card>
