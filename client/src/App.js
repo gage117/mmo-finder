a6bb0f3dde191252df4628ffd733cab60ad5ef4d
Gage Eide <gageeide@gmail.com>
2021-09-06 21:52:39 -0500

    Added SearchBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import GameGrid from './components/GameGrid';
 import NavBar from './components/NavBar';
+import SearchBar from './components/SearchBar';
 import API from './utils/api';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <SearchBar />
       <GameGrid />
     </div>
   );
