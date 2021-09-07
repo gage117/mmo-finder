cc394f3aede34f3288ce7cfc6eed61ac0ba132ec
Gage Eide <gageeide@gmail.com>
2021-09-06 20:01:44 -0500

    Added NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import GameGrid from './components/GameGrid';
+import NavBar from './components/NavBar';
 import API from './utils/api';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <GameGrid />
     </div>
   );
