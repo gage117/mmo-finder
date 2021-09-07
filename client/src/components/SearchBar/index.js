14850fef0abf6bd8844b2fdbb77dc5fb7a7c9530
Gage Eide <gageeide@gmail.com>
2021-09-06 23:19:37 -0500

    Added state to chips

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,34 +1,53 @@
-import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Chip } from '@material-ui/core';
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Chip } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: '#3F51B5',
-    padding: '15px 0px',
-    '& > *': {
+    backgroundColor: "#3F51B5",
+    padding: "15px 0px",
+    "& > *": {
       margin: theme.spacing(1),
     },
   },
   inputBackground: {
-      backgroundColor: '#7E88C1',
-      width: '25ch',
+    backgroundColor: "#7E88C1",
+    width: "25ch",
   },
   chipMargin: {
-      margin: '0 5px'
-  }
+    margin: "0 5px",
+  },
 }));
 
-export default function SearchBar({tags}) {
+export default function SearchBar({ tags }) {
   const classes = useStyles();
+  const [activeTags, setActiveTags] = useState([]);
+
+  const handleChipClick = (e) => {
+    activeTags.includes(e.target.innerText) ?
+      setActiveTags([e.target.innerText, ...activeTags])
+    : setActiveTags(activeTags.filter(tag => tag !== e.target.innerText))
+  };
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField className={classes.inputBackground} id="outlined-basic" label="Game Title" variant="filled" />
+      <TextField
+        className={classes.inputBackground}
+        id="outlined-basic"
+        label="Game Title"
+        variant="filled"
+      />
       <div>
-        {tags.map(tag => {
-            return (
-                <Chip label={tag} key={tag} className={classes.chipMargin}/>
-            )
+        {tags.map((tag) => {
+          return (
+            <Chip
+              label={tag}
+              key={tag}
+              className={classes.chipMargin}
+              onClick={handleChipClick}
+              color={activeTags.includes(tag) ? 'primary' : 'default'}
+            />
+          );
         })}
       </div>
     </form>
