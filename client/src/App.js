0f635d28ff4d64e5a29a6d4b8b9ddb232fc9c373
Gage Eide <gageeide@gmail.com>
2021-09-06 22:05:21 -0500

    Moved SearchBar Render

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import GameGrid from './components/GameGrid';
 import NavBar from './components/NavBar';
-import SearchBar from './components/SearchBar';
 import API from './utils/api';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <SearchBar />
       <GameGrid />
     </div>
   );
