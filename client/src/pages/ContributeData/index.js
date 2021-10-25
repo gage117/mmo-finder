3a6fa7878cb4e26cd1b9e6569008c91fa081f5ed
gage117 <57643501+gage117@users.noreply.github.com>
2021-10-24 21:57:28 -0500

    Stopping for the night

--- client/src/pages/ContributeData/index.js
+++ client/src/pages/ContributeData/index.js
@@ -1,7 +1,22 @@
+import {Container,
+  Typography,
+  TextField,
+  Button
+} from '@material-ui/core';
 import ContributeDataForm from '../../components/ContributeDataForm';
 
 export default function ContributeData() {
   return (
-    <ContributeDataForm />
+  <Container>
+    <Typography variant="h2">Contribute Data</Typography>
+    <Container>
+      <Typography variant="h4">Add data about an existing game</Typography>
+      <TextField label="Game Name" />
+    </Container>
+    <Container>
+      <Typography variant="h4">Add data about a new game</Typography>
+      <Button variant="contained" color="primary" >Add Game</Button>
+    </Container>
+  </Container>
   )
 }
