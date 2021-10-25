f0597cca3c3dfa7092b698aea0ab04c3e8e92395
gage117 <57643501+gage117@users.noreply.github.com>
2021-10-24 21:34:00 -0500

    Added Basis for Contribute Data page

--- client/src/components/ContributeDataForm/index.js
+++ client/src/components/ContributeDataForm/index.js
@@ -1,7 +1,21 @@
-
+import {Container,
+        Typography,
+        TextField,
+        Button
+} from '@material-ui/core';
 
 export default function ContributeDataForm() {
   return (
-    <div>Bark</div>
+    <Container>
+      <Typography variant="h2">Contribute Data</Typography>
+      <Container>
+        <Typography variant="h4">Add data about an existing game</Typography>
+        <TextField label="Game Name" />
+      </Container>
+      <Container>
+        <Typography variant="h4">Add data about a new game</Typography>
+        <Button variant="contained" color="primary" >Add Game</Button>
+      </Container>
+    </Container>
   )
 }
