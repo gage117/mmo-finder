5e6d72379eb5bf305c81f786326d7115167688de
gage117 <57643501+gage117@users.noreply.github.com>
2021-12-17 17:46:32 -0600

    Turned request into async/await to help with readability

--- server.js
+++ server.js
@@ -12,27 +12,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
-	.then(res => {
-		axios({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
-			method: "POST",
-			url: "https://api.igdb.com/v4/games",
-			headers: {
-					'Client-ID': process.env.IGDB_CLIENT_ID,
-					Authorization: `Bearer ${res.data.access_token}`,
-			},
-			data: "fields *, keywords.name; where game_modes = [5];"
-		})
-		.then(res => {
-			console.log(res.data);
-		})
-		.catch(err => {
-			console.log(err);
-		})
-	})
-	.catch(err => {
-		console.log(err);
+(async () => {
+	const tokenRes = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
+	const gamesRes = await axios({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
+		method: "POST",
+		url: "https://api.igdb.com/v4/games",
+		headers: {
+				'Client-ID': process.env.IGDB_CLIENT_ID,
+				Authorization: `Bearer ${tokenRes.data.access_token}`,
+		},
+		data: "fields *, keywords.name; where game_modes = [5];"
 	});
+	console.log(gamesRes.data);
+})()
 
 const config = {
 	useNewUrlParser: true,
