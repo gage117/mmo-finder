6791e2cca3eb021fc0c9bedce13387bd1e600a17
gage117 <57643501+gage117@users.noreply.github.com>
2021-12-17 21:03:53 -0600

    Added default headers to axios so authorization is used on all IGDB requests

--- server.js
+++ server.js
@@ -14,13 +14,11 @@ app.use(express.static("public"));
 
 (async () => {
 	const tokenRes = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
+	axios.defaults.headers.common['Authorization'] = `Bearer ${tokenRes.data.access_token}`;
+	axios.defaults.headers.common['Client-ID'] = process.env.IGDB_CLIENT_ID;
 	const gamesRes = await axios({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
 		method: "POST",
 		url: "https://api.igdb.com/v4/games",
-		headers: {
-				'Client-ID': process.env.IGDB_CLIENT_ID,
-				Authorization: `Bearer ${tokenRes.data.access_token}`,
-		},
 		data: "fields *, keywords.name; where game_modes = [5];"
 	});
 	console.log(gamesRes.data);
