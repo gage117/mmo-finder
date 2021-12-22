2991bd3ecbf1e02e932abce604d693b78119f5d9
gage117 <57643501+gage117@users.noreply.github.com>
2021-12-22 13:27:32 -0600

    Added start of GamePage component

--- client/src/pages/GamePage/index.js
+++ client/src/pages/GamePage/index.js
@@ -1,5 +1,17 @@
+import { useState, useEffect } from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "0px 15px",
+    backgroundColor: "#fafafa",
+    height: "100%",
+  },
+}));
+
 export default function GamePage() {
+  const classes = useStyles();
   return (
-    <div>GENERAL KENOBI</div>
+    <div className={classes.root}>GENERAL KENOBI</div>
   )
 }
