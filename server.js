843ade2e99c27da6d10364f71df0d9d3ba05b24d
gage117 <57643501+gage117@users.noreply.github.com>
2021-12-22 11:57:36 -0600

    Changed wildcard origin to localhost

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mmo_finder", co
 
 // CORS headers
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
