e519f86d120217e1297292bad2ef9fe1d70b9cb6
gage117 <57643501+gage117@users.noreply.github.com>
2021-12-22 13:43:36 -0600

    Added loading div if gameData hasn't been retrieved yet

--- client/src/pages/GamePage/index.js
+++ client/src/pages/GamePage/index.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import API from '../../utils/api';
 import { makeStyles } from "@material-ui/core/styles";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,8 +13,22 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function GamePage() {
+  const [ gameData, setGameData ] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    API.get(`/mmo/${params.game}`).then( res => setGameData(res.data) )
+  }, [])
+  
   const classes = useStyles();
+
   return (
-    <div className={classes.root}>GENERAL KENOBI</div>
+    <div className={classes.root}>
+      {!gameData ?
+        <div> Loading... </div> 
+        :
+        <div>{gameData.name}</div>
+      }
+    </div>
   )
 }
