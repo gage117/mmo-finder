14f80278f841fa4f49fbc9740f7538b23a4a484c
gage117 <57643501+gage117@users.noreply.github.com>
2022-01-01 16:27:40 -0600

    Removed unnecessary variable declaration on API import in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const IGDB_api = require("./igdb-api");
+require("./igdb-api");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
