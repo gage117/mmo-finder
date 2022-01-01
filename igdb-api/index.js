2d5ff134f5cc971fdf31ef40c1d4e638e4172e55
gage117 <57643501+gage117@users.noreply.github.com>
2022-01-01 16:30:41 -0600

    Cleaned up index.js of igdb-api

--- igdb-api/index.js
+++ igdb-api/index.js
@@ -5,26 +5,20 @@ const initializeIGDBapi = async () => {
 	const tokenRes = await IGDB_api.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
 	IGDB_api.defaults.headers.common['Authorization'] = `Bearer ${tokenRes.data.access_token}`;
 	IGDB_api.defaults.headers.common['Client-ID'] = process.env.IGDB_CLIENT_ID;
-	// const gamesRes = await IGDB_api({ // IGDB_api.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
-	// 	method: "POST",
-	// 	url: "https://api.igdb.com/v4/games",
-	// 	data: "fields *, keywords.name; where game_modes = [5];"
-	// });
-	// console.log(gamesRes.data);
 };
 
 initializeIGDBapi();
 
 // Custom methods:
 IGDB_api.getAllMMOGames = () => {
-  return IGDB_api.request({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
+  return IGDB_api.request({
     method: "POST",
     url: "https://api.igdb.com/v4/games",
     data: "fields *, keywords.name; where game_modes = [5];"
   }) 
 }
 IGDB_api.getGameByName = (gameName) => {
-  return IGDB_api.request({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
+  return IGDB_api.request({
     method: "POST",
     url: "https://api.igdb.com/v4/games",
     data: `fields *, keywords.name; where name = "${gameName}";`
@@ -32,7 +26,7 @@ IGDB_api.getGameByName = (gameName) => {
 }
 //! TODO: Implement in API.js for GET /api/mmo/:id
 IGDB_api.getGameByID = (gameID) => {
-  return IGDB_api.request({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
+  return IGDB_api.request({
     method: "POST",
     url: "https://api.igdb.com/v4/games",
     data: `fields *, keywords.name; where id = ${gameID};`
