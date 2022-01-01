3240ff6cdc68f0b82a007ea1dde8bdea5eb1aa2c
gage117 <57643501+gage117@users.noreply.github.com>
2022-01-01 16:28:00 -0600

    Added some custom methods to IGDB axios

--- igdb-api/index.js
+++ igdb-api/index.js
@@ -1,18 +1,42 @@
-const axios = require('axios');
+const IGDB_api = require('axios');
 require("dotenv").config();
 
 const initializeIGDBapi = async () => {
-	const tokenRes = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
-	axios.defaults.headers.common['Authorization'] = `Bearer ${tokenRes.data.access_token}`;
-	axios.defaults.headers.common['Client-ID'] = process.env.IGDB_CLIENT_ID;
-	const gamesRes = await axios({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
-		method: "POST",
-		url: "https://api.igdb.com/v4/games",
-		data: "fields *, keywords.name; where game_modes = [5];"
-	});
-	console.log(gamesRes.data);
+	const tokenRes = await IGDB_api.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
+	IGDB_api.defaults.headers.common['Authorization'] = `Bearer ${tokenRes.data.access_token}`;
+	IGDB_api.defaults.headers.common['Client-ID'] = process.env.IGDB_CLIENT_ID;
+	// const gamesRes = await IGDB_api({ // IGDB_api.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
+	// 	method: "POST",
+	// 	url: "https://api.igdb.com/v4/games",
+	// 	data: "fields *, keywords.name; where game_modes = [5];"
+	// });
+	// console.log(gamesRes.data);
 };
 
 initializeIGDBapi();
 
-module.exports = { axios };
+// Custom methods:
+IGDB_api.getAllMMOGames = () => {
+  return IGDB_api.request({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
+    method: "POST",
+    url: "https://api.igdb.com/v4/games",
+    data: "fields *, keywords.name; where game_modes = [5];"
+  }) 
+}
+IGDB_api.getGameByName = (gameName) => {
+  return IGDB_api.request({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
+    method: "POST",
+    url: "https://api.igdb.com/v4/games",
+    data: `fields *, keywords.name; where name = "${gameName}";`
+  }) 
+}
+//! TODO: Implement in API.js for GET /api/mmo/:id
+IGDB_api.getGameByID = (gameID) => {
+  return IGDB_api.request({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
+    method: "POST",
+    url: "https://api.igdb.com/v4/games",
+    data: `fields *, keywords.name; where id = ${gameID};`
+  })
+}
+
+module.exports = { IGDB_api };
