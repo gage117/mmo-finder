b883265bc58907688a44b68a7ee53837fcad2f44
gage117 <57643501+gage117@users.noreply.github.com>
2022-01-01 15:51:07 -0600

    Modularized IGDB api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const axios = require("axios");
+const IGDB_api = require("./igdb-api");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
@@ -11,18 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-(async () => {
-	const tokenRes = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
-	axios.defaults.headers.common['Authorization'] = `Bearer ${tokenRes.data.access_token}`;
-	axios.defaults.headers.common['Client-ID'] = process.env.IGDB_CLIENT_ID;
-	const gamesRes = await axios({ // axios.post not working with this request, Need to use object with URL and method properties instead to get an authorized response
-		method: "POST",
-		url: "https://api.igdb.com/v4/games",
-		data: "fields *, keywords.name; where game_modes = [5];"
-	});
-	console.log(gamesRes.data);
-})()
 
 const config = {
 	useNewUrlParser: true,
