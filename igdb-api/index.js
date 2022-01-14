33ba3eaa365fc9274cde29c084618489f3b3fbd2
gage117 <57643501+gage117@users.noreply.github.com>
2022-01-14 14:27:03 -0600

    Grabbing more fields from IGDB

--- igdb-api/index.js
+++ igdb-api/index.js
@@ -5,23 +5,22 @@ const initializeIGDBapi = async () => {
 	const tokenRes = await IGDB_api.post(`https://id.twitch.tv/oauth2/token?client_id=${process.env.IGDB_CLIENT_ID}&client_secret=${process.env.IGDB_CLIENT_SECRET}&grant_type=client_credentials`)
 	IGDB_api.defaults.headers.common['Authorization'] = `Bearer ${tokenRes.data.access_token}`;
 	IGDB_api.defaults.headers.common['Client-ID'] = process.env.IGDB_CLIENT_ID;
+  console.log("IGDB API initialized");
 };
-
-initializeIGDBapi();
 
 // Custom methods:
 IGDB_api.getAllMMOGames = () => {
   return IGDB_api.request({
     method: "POST",
     url: "https://api.igdb.com/v4/games",
-    data: "fields *, keywords.name; where game_modes = [5];"
+    data: "fields *, keywords.name, cover.url; where game_modes = [5];"
   }) 
 }
 IGDB_api.getGameByName = (gameName) => {
   return IGDB_api.request({
     method: "POST",
     url: "https://api.igdb.com/v4/games",
-    data: `fields *, keywords.name; where name = "${gameName}";`
+    data: `fields *, keywords.name, cover.url; where name = "${gameName}";`
   }) 
 }
 //! TODO: Implement in API.js for GET /api/mmo/:id
@@ -29,8 +28,8 @@ IGDB_api.getGameByID = (gameID) => {
   return IGDB_api.request({
     method: "POST",
     url: "https://api.igdb.com/v4/games",
-    data: `fields *, keywords.name; where id = ${gameID};`
+    data: `fields *, keywords.name, cover.url; where id = ${gameID};`
   })
 }
 
-module.exports = { IGDB_api };
+module.exports = { IGDB_api, initializeIGDBapi };
